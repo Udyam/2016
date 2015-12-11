9c1b8b4c4d9c03f19d7c5551677d1673168d5eb4
DarKnight <05.DarKnight.11@gmail.com>
2015-12-11 15:44:22 +0530

    minor changes in register view

--- Udyam/views.py
+++ Udyam/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from forms import RegistrationForm
 from models import Events
 from django.utils import timezone
 
 # Create your views here.
-
-
-def check_mail(data):
-    temp = dict()
-    for entry in data:
-        
 
 
 def index(request):
@@ -18,16 +12,21 @@ def index(request):
 
 def register(request, name):
     if request.method == 'POST':
+        # request.POST = request.POST.copy()
+        # request.POST['event_name'] = name
         form = RegistrationForm(request.POST)
+        print form
         if form.is_valid():
             model_instance = form.save(commit=False)
             model_instance.timestamp = timezone.now()
             model_instance.save()
             # TODO return success page
             # TODO check for the repeated email entry
+        else:
+            return HttpResponse()
     form = RegistrationForm()
     event = Events.objects.get(name=name)
-    return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form, 'event':event})
 
 
 def events(request, name):
