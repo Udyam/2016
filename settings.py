5f04839443693bb0dbe9f90b7aa2bff4003d65ad
DarKnight <05.DarKnight.11@gmail.com>
2015-12-11 20:01:37 +0530

    added dynamic capability to register template

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'Udyam2016', 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
